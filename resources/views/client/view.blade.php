58ca1715de1ec490ad571c1a2634ef4261c56467
AGrant24 <alex.grant.contact@gmail.com>
2021-11-17 18:09:46 +0000

    moved edit button to the right

--- resources/views/client/view.blade.php
+++ resources/views/client/view.blade.php
@@ -2,19 +2,20 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-{{-- Action card --}}
+{{-- Back Button --}}
 
+{{-- Action card --}}
     <div class="row">
         <div class="col-sm-4">
-          
             <div class="card" id="action-card">
               <div class="card-body">
                 <h1 class="card-title">{{ $clients->client_name }}</h1>
                 <p class="card-text">Client Reference: {{ str_pad($clients->id, 6, '0', STR_PAD_LEFT) }}</p>
-                <a role="button" class="btn btn-primary" href="{{ url('client/edit/'.$clients->id) }}">Edit</a>
+                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <a role="button" class="btn btn-primary" href="{{ url('client/edit/'.$clients->id) }}">Edit</a>
+                  </div>
               </div>
             </div>
-          
         </div>
      </div>
 
